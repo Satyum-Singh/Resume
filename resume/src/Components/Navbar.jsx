9d3dacd0afc8711cf33d333a881b6b2a99d4b06d
Satyum <satyumspathania@gmail.com>
2023-08-03 17:23:11 +0530

    Setting up tailwindCSS.

--- resume/src/Components/Navbar.jsx
+++ resume/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <h1>Satyum Singh Pathania</h1>
+            <h1 className="text-3xl text-blue-500 font-bold underline">Satyum Singh Pathania</h1>
         </div>
     )
 }
