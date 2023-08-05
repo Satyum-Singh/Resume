d65780c1c4bbbd035c0a67966bd18652fd8f7a4f
Satyum <satyumspathania@gmail.com>
2023-08-05 20:27:17 +0530

    Working on navbar.

--- resume/src/Components/Navbar.jsx
+++ resume/src/Components/Navbar.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='bg-wall-sec w-100vw h-48'>
-            <button>Contact Me</button>
+        <div>
+            <div className='flex bg-wall-sec w-100vw  text-skin-base justify-between px-32 py-10'>
+                <div><h1 className='text-4xl text-white font-semibold'>SatyumSP</h1></div>
+                <div className='flex space-x-10'>
+                    <p className='w-fill px-4 py-2 bg-red-500 hover:border-2 hover'><a href="">Home</a></p>
+                    <p><a href="">Projects</a></p>
+                    <p><a href="">About</a></p>
+                    <p><a href="">Contact</a></p>
+                </div>
+                <div className='flex space-x-10'>
+                    <div>LinkedIn</div>
+                    <div>Github</div>
+                    <div>Instagram</div>
+                    <div>Leetcode</div>
+                </div>
+            </div>
         </div>
     )
 }
