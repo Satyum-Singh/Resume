f896cbb147a9417b11744edc75dd6ef78112457d
Satyum <satyumspathania@gmail.com>
2023-08-11 00:00:55 +0530

    working on brand.

--- resume/src/Components/Navbar.jsx
+++ resume/src/Components/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
         <div>
             <div className='flex bg-wall-sec w-100vw  text-skin-base justify-between px-32 py-10'>
                 <div><h1 className='text-4xl text-white font-semibold'>SatyumSP</h1></div>
+                <div></div>
                 <div className='flex space-x-10'>
                     <p className='w-fill px-4 py-2 bg-blue-500 hover:border-2 hover'><a href="">Home</a></p>
                     <p><a href="">Projects</a></p>
